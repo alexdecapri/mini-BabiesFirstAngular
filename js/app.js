2593e97d39895e67240e839ea5d940dd45b6c453
alexdecapri <alexdecapri@gmail.com>
2016-02-18 18:45:07 -0700

    service and factory notes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 var app = angular.module('friendsList', []);
+
+/*
+app.service("friendService", function() {
+	this.foo = "bar";
+});
+*/
+
+// this service and factory do the exact same thing, but how you have to type them is different
+
+/*
+app.factory("friendFacService", function() {
+	return { foo: "bar" };
+});
+*/
